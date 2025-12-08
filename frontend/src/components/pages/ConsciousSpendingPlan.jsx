c2c668bfd5b08ab9f334b896cd62ae8db5e9458c
Phil Getzen <getzenph@gmail.com>
2025-12-08 15:33:56 -0800

    Add 1 month filter option to CSP period selector

--- frontend/src/components/pages/ConsciousSpendingPlan.jsx
+++ frontend/src/components/pages/ConsciousSpendingPlan.jsx
@@ -65,6 +65,7 @@ const BUCKET_ICONS = {
 
 // Period options
 const PERIOD_OPTIONS = [
+  { value: 1, label: '1 Month' },
   { value: 3, label: '3 Months' },
   { value: 6, label: '6 Months' },
   { value: 12, label: '12 Months' },
